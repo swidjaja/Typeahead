4f296549211996a1adff21744b4eb40f88070c5a
sugi <widjaja.s@gmail.com>
2020-03-07 11:32:27 -0800

    Updates

---
+++ precache-manifest.7e47d29c0a71c2b61452b1e12dbeb5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aedd812baabf2238e78f43633a9644d7",
+    "url": "/Typeahead/index.html"
+  },
+  {
+    "revision": "3962cd22e5471468dde5",
+    "url": "/Typeahead/static/css/main.23280a3a.chunk.css"
+  },
+  {
+    "revision": "a8fe716098d5abf6d75b",
+    "url": "/Typeahead/static/js/2.4f7ccc51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Typeahead/static/js/2.4f7ccc51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3962cd22e5471468dde5",
+    "url": "/Typeahead/static/js/main.70a695ff.chunk.js"
+  },
+  {
+    "revision": "119ebb1a4bc3086901a4",
+    "url": "/Typeahead/static/js/runtime-main.475ee91f.js"
+  }
+]);
